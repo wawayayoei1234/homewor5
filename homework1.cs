cd3c8f2139428a23a244462558b7290abcc7577f
wawa <tanwa5120@gmail.com>
2019-11-15 05:12:26 +0700

    finalcode

--- homework1.cs
+++ homework1.cs
@@ -1,21 +1,26 @@
+using System;
 public class homewark : IHomework05
+{
+    public static string[] led = { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+    public string DisplayLEDOnScreen(string ledNo)
     {
-        public static string[] led = { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
-        public string DisplayLEDOnScreen(string ledNo)
-        {   
-            string sum = null;
-            if(ledNo=="A")
-            {
-            led[9] = led[9]=="[ ]"?"[!]":"[ ]";
-            }
-            else
-            {
-               int numled = int.Parse(ledNo);
-                numled--;
-                led[numled]=led[numled]=="[ ]"?"[!]":"[ ]";
-            }
-            foreach (string item in led)
-            sum+=item;
-            return sum;   
+        string sum = null;
+        if (ledNo == "A")
+        {
+            led[9] = led[9] == "[ ]" ? "[!]" : "[ ]";
         }
-    }   
+        else
+        {
+            int numled = int.Parse(ledNo);
+            numled--;
+            led[numled] = led[numled] == "[ ]" ? "[!]" : "[ ]";
+        }
+        foreach (string item in led)
+        {
+            sum += item;
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine(" 1  2  3  4  5  6  7  8  9  A");
+        return sum;
+    }
+}
